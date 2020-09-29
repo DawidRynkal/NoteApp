0c8e6787eb8ec60345f38cb7f07244d63c8d506e
Dawid Rynkal <dawidos14051997@gmail.com>
2020-09-29 16:52:02 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+class function App() {
   return (
     <div className="App">
       <header className="App-header">
